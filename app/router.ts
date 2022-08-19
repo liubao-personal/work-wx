2031f561d00bc6e15a9c1aa7952cb3cc8e0749a5
刘抱 <bao.liu@medsci.cn>
2022-08-19 10:32:52 +0800

    refactor: update send msg router method

--- app/router.ts
+++ app/router.ts
@@ -7,7 +7,7 @@ export default (app: Application) => {
   router.get('/', controller.home.index);
   /* 企业微信api start */
   router.get('/work/getToken', controller.work.getToken);
-  router.get('/work/send', workAccessToken, controller.work.send);
+  router.all('/work/send', workAccessToken, controller.work.send);
   router.get('/work/authorize', workAccessToken, controller.work.authorize); // 企业微信网页授权
   router.get('/work/back', workAccessToken, controller.work.back); // 授权后回调地址
   router.post('/work/recall', workAccessToken, controller.work.recall); // 撤回应用消息
